8e5c7c5d7b03f625363cf0abc682765abc922cf1
Kumaran M <kumaran.developer@gmail.com>
2017-12-28 21:17:17 +0530

    Reddit Data Fetch using redux
    Redux implementation of reddit posts fetch

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import reducer from './reducer'
+import './index.css';
+import App from './container/App';
+import registerServiceWorker from './registerServiceWorker';
+
+const middleware = [ thunk ]
+if(process.env.NODE_ENV !== 'production'){
+  middleware.push(createLogger());
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middleware)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+document.getElementById('root'));
+registerServiceWorker();
